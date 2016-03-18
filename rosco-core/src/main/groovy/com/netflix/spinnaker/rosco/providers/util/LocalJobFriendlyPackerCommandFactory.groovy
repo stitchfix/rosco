09f9803a7b99c7a590c76d761b946947d939968d
Diana Tkachenko <dtkachenko@stitchfix.com>
2016-03-18 14:55:24 -0700

    Revert "adding sudo to packer command"
    This reverts commit 6261293de2fa3b9bd5d961d9db2ace0029b0e808.

--- rosco-core/src/main/groovy/com/netflix/spinnaker/rosco/providers/util/LocalJobFriendlyPackerCommandFactory.groovy
+++ rosco-core/src/main/groovy/com/netflix/spinnaker/rosco/providers/util/LocalJobFriendlyPackerCommandFactory.groovy
@@ -20,7 +20,7 @@ class LocalJobFriendlyPackerCommandFactory implements PackerCommandFactory {
 
   @Override
   List<String> buildPackerCommand(String baseCommand, Map<String, String> parameterMap, String absoluteTemplateFilePath) {
-    def packerCommand = ["sudo", "packer", "build", "-color=false"]
+    def packerCommand = ["packer", "build", "-color=false"]
 
     parameterMap.each { key, value ->
       if (key && value) {
